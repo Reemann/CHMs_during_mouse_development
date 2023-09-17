e4e9fd2732069e4478738bd685296a73dff0f20f
Reemann <2011419@tongji.edu.cn>
2023-09-17 17:31:01 +0800

    rename twice

--- README.md
+++
@@ -1,5 +0,0 @@
-# CHMs_during_mouse_development
-Codes for "**Characterization of H3K9me3 and DNA methylation co-marked CpG-rich regions during mouse development**".
-
-Citation:
-**Yang H\*, Wang Y\*, Zhang Y$**. 2023. Characterization of H3K9me3 and DNA methylation co-marked CpG-rich regions during mouse development. (Under review)
--- make17_pcar_test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd ~/CHMsInOtherContexts/CellStateTransition/pcar_test
-pcar -m callchm -H E11.5.H3K9me3.rep1.bam -C E11.5.input.rep1.bam -M E11.5.WGBS.rep1.sam.G.bed -Z /mnt/Storage/home/yanghui/annotations/mm10/mm10_euch.chrom.sizes -Q /mnt/Storage/home/yanghui/annotations/mm10/mm10.2bit -P 0.005 -D 'CH-nonM' -N test_P0p005_CHnonM
-echo "done"
